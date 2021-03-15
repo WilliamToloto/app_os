349e20730f830e7e6baf4cefffe36c2d63fdf415
renatojabreu <renatoj_abreu@hotmail.com>
2021-03-15 16:26:56 -0300

    ok

---
+++ lib/control/func.dart
@@ -0,0 +1,23 @@
+import 'package:app_novo/model/produtoOs.dart';
+
+Future loadProdutos1(ProdutosList produtosList, response, produtosList1,
+    dataExpirada, osCod) async {
+  ProdutosList produtosList = ProdutosList.fromJson(response.data);
+  print(produtosList.produtos[0].qtd);
+  print(produtosList.produtos.length);
+  produtosList1 = produtosList.produtos;
+  print(produtosList1[0].desc);
+  print(produtosList1[0].cod_produto);
+  print(produtosList1[0].codProd);
+
+  String dataPrevisao = produtosList.produtos[0].dataPrevisao;
+  DateTime.parse(dataPrevisao);
+  print(dataPrevisao);
+  if (DateTime.now().isBefore(DateTime.parse(dataPrevisao))) {
+    print("eh menor");
+    dataExpirada = "n";
+  } else {
+    dataExpirada = "s";
+  }
+  osCod = produtosList.produtos[0].codOs;
+}
